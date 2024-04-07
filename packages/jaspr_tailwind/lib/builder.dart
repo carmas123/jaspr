350a592fd433c060f95a5283cfef1b7ee162e6d4
carmas123 <m.caroccia83@gmail.com>
2024-04-07 19:02:05 +0200

    Fix Tailwind builder under Windows with extensions

--- packages/jaspr_tailwind/lib/builder.dart
+++ packages/jaspr_tailwind/lib/builder.dart
@@ -17,12 +17,15 @@ class TailwindBuilder implements Builder {
 
     await scratchSpace.ensureAssets({buildStep.inputId}, buildStep);
 
-    var outputId = buildStep.inputId.changeExtension('').changeExtension('.css');
+    var outputId =
+        buildStep.inputId.changeExtension('').changeExtension('.css');
 
     var packageFile = File('.dart_tool/package_config.json');
     var packageJson = jsonDecode(await packageFile.readAsString());
 
-    var packageConfig = (packageJson['packages'] as List?)?.where((p) => p['name'] == 'jaspr_tailwind').firstOrNull;
+    var packageConfig = (packageJson['packages'] as List?)
+        ?.where((p) => p['name'] == 'jaspr_tailwind')
+        .firstOrNull;
     if (packageConfig == null) {
       print("Cannot find 'jaspr_tailwind' in package config.");
       return;
@@ -32,7 +35,8 @@ class TailwindBuilder implements Builder {
     var assets = await buildStep.findAssets(Glob('{lib,web}/**.dart')).toList();
     await Future.wait(assets.map((a) => buildStep.canRead(a)));
 
-    var root = p.normalize(p.join(Directory.current.path, '.dart_tool', Uri.parse(packageConfig['rootUri']!).path));
+    var root = p.normalize(p.join(Directory.current.path, '.dart_tool',
+        Uri.parse(packageConfig['rootUri']!).path));
 
     var configFile = File('tailwind.config.js');
     var hasCustomConfig = await configFile.exists();
@@ -44,9 +48,11 @@ class TailwindBuilder implements Builder {
         '--input',
         scratchSpace.fileFor(buildStep.inputId).path,
         '--output',
-        fixPathWindows(scratchSpace.fileFor(outputId).path),
+        scratchSpace.fileFor(outputId).path.toPosix(),
         '--content',
-        quotePathIfNeeded(p.join(Directory.current.path, '{lib,web}', '**', '*.dart')),
+        p
+            .join(Directory.current.path, '{lib,web}', '**', '*.dart')
+            .toPosix(true),
         if (hasCustomConfig) ...[
           '--config',
           p.join(Directory.current.path, 'tailwind.config.js'),
@@ -63,20 +69,14 @@ class TailwindBuilder implements Builder {
   Map<String, List<String>> get buildExtensions => {
         'web/{{file}}.tw.css': ['web/{{file}}.css']
       };
+}
 
-  String fixPathWindows(String path) {
-    var result = path;
+extension POSIXPath on String {
+  String toPosix([bool quoted = false]) {
     if (Platform.isWindows) {
-      result = result.replaceAll("\\", "/");
+      final result = replaceAll('\\', '/');
+      return quoted ? "'$result'" : result;
     }
-    return result;
-  }
-
-  String quotePathIfNeeded(String path) {
-    var result = path;
-    if (Platform.isWindows) {
-      result = "'${fixPathWindows(result)}'";
-    }
-    return result;
+    return this;
   }
 }
